aa2a0a806bcc172c68563bf206f2d3b94c54e2eb
justmahmoud31 <justmahmoud3152003@gmail.com>
2025-05-27 19:50:42 +0300

    Made the server Bigger

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,8 @@ const app = express();
 const prisma = new PrismaClient();
 configDotenv();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Health check route
 app.get('/', (req, res) => {
